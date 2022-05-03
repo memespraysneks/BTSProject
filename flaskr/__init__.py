cbe14a5d179bc62284112796995f609eec38b418
Liam Brooke <24947193+ItsLimaBean@users.noreply.github.com>
2022-05-03 09:15:55 -0700

    Add basic week view

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -13,14 +13,33 @@ def create_app(test_config=None):
 
     @app.route("/month")
     def month():
-        return redirect(f"/month/{datetime.date.today().isoformat()}")
+        current_date = datetime.date.today()
+        return redirect(f"/month/{current_date.year}-{current_date.month}")
 
-    @app.route("/month/<date>")
-    def month_date(date):
-        calendar_date = datetime.date.fromisoformat(date)
+    @app.route("/month/<int:year>-<int:month>")
+    def month_date(year, month):
+        month_name = datetime.datetime(year, month, 1).strftime("%B")
+
         calendar.setfirstweekday(6)
-        month_calendar = calendar.monthcalendar(calendar_date.year, calendar_date.month)
-        return render_template("month.html", month_data=month_calendar, month_name=calendar_date.strftime("%B"))
+        month_calendar = calendar.monthcalendar(year, month)
+        
+        return render_template("month.html", month_id=f"{year}-{month}", month_data=month_calendar, month_name=month_name)
+
+    @app.route("/week")
+    def week():
+        current_date = datetime.date.today()
+        return redirect(f"/week/{current_date.year}-{current_date.month}-0")
+
+    @app.route("/week/<int:year>-<int:month>-<int:week>")
+    def week_date(year, month, week):
+        month_name = datetime.datetime(year, month, 1).strftime("%B")
+
+        calendar.setfirstweekday(6)
+        month_calendar = calendar.monthcalendar(year, month)
+        week %= len(month_calendar) 
+
+        week_dates = month_calendar[week] # Hacky workaround to prevent invalid week
+        return render_template("week.html", week=week_dates, month_name=month_name, week_num=week)
 
     return app
 
