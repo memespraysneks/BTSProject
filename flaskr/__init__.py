a0a3725b4240bbaf76e2a2bde403f6f5ae6c9878
ItsLimaBean <ItsLimaBean@users.noreply.github.com>
2022-05-02 21:37:09 -0700

    Basic month calendar layout.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,8 @@
 import os
 import flask
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
+import datetime
+import calendar
 
 
 def create_app(test_config=None):
@@ -8,6 +10,17 @@ def create_app(test_config=None):
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route("/month")
+    def month():
+        return redirect("/month/"+datetime.date.today().isoformat())
+
+    @app.route("/month/<date>")
+    def month_date(date):
+        calendar_date = datetime.date.fromisoformat(date)
+        month_calendar = calendar.monthcalendar(calendar_date.year, calendar_date.month)
+        return render_template("month.html", month_data=month_calendar, month_name=calendar_date.strftime("%B"))
+
     return app
 
 if __name__ == "__main__":
