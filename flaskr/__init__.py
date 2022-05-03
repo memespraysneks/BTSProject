8ada35851d102787ef4c389fc3ef8b5c4872c695
alsomemespraysnek <calebsjseeman@gmail.com>
2022-05-02 22:01:53 -0700

    Added a database and linked it to the flask framework

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,14 +1,16 @@
 import os
 import flask
 from flask import Flask, render_template
-
+from database import databaseAPI
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.register_blueprint(databaseAPI)
     @app.route("/")
     def index():
         return render_template("index.html")
     return app
+
 
 if __name__ == "__main__":
     create_app()
