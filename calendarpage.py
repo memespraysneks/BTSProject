5f3a00b6c03fbc8796725e3e2c11ec33d3988e78
ItsLimaBean <ItsLimaBean@users.noreply.github.com>
2022-05-03 20:57:58 -0700

    Add function to get month data.

--- calendarpage.py
+++ calendarpage.py
@@ -4,6 +4,14 @@ from flask import Flask, redirect, render_template, url_for, Blueprint
 
 calendarpage = Blueprint("calendarpage", __name__)
 
+def get_month_data(year, month):
+    month_name = datetime.datetime(year, month, 1).strftime("%B")
+
+    calendar.setfirstweekday(6)
+    month_calendar = calendar.monthcalendar(year, month)
+
+    return (month_name, month_calendar)
+
 @calendarpage.route("/month")
 def month():
     current_date = datetime.date.today()
@@ -11,10 +19,7 @@ def month():
 
 @calendarpage.route("/month/<int:year>-<int:month>")
 def month_date(year, month):
-    month_name = datetime.datetime(year, month, 1).strftime("%B")
-
-    calendar.setfirstweekday(6)
-    month_calendar = calendar.monthcalendar(year, month)
+    month_name, month_calendar = get_month_data(year, month)
     
     return render_template("month.html", month_id=f"{year}-{month}", month_data=month_calendar, month_name=month_name)
 
@@ -25,10 +30,7 @@ def week():
 
 @calendarpage.route("/week/<int:year>-<int:month>-<int:week>")
 def week_date(year, month, week):
-    month_name = datetime.datetime(year, month, 1).strftime("%B")
-
-    calendar.setfirstweekday(6)
-    month_calendar = calendar.monthcalendar(year, month)
+    month_name, month_calendar = get_month_data(year, month)
     week %= len(month_calendar) # Hacky workaround to prevent invalid week
 
     week_dates = month_calendar[week]
