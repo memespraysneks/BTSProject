2fe78931ce75060d4038a67478d57a7047e883cd
ItsLimaBean <ItsLimaBean@users.noreply.github.com>
2022-05-03 20:18:05 -0700

    Use embedded script tag instead of js file.

--- flaskr/static/calendar_script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".week_wrap").addEventListener("click", (event) => {
-    let weekid = event.target.closest(".week").dataset.weekid
-    if (weekid) {
-        window.location = `/week/${weekid}`
-    }
-})
