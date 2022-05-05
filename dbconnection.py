15c5338bb7c63f33d18d605188ee7ac197542a72
Liam Brooke <24947193+ItsLimaBean@users.noreply.github.com>
2022-05-05 10:55:50 -0700

    Actually read the file

--- dbconnection.py
+++ dbconnection.py
@@ -3,7 +3,7 @@ import sqlite3
 def setup_db():
     db = get_db()
     with open("database.sql") as sql_file:
-        db.cursor().executescript(sql_file)
+        db.cursor().executescript(sql_file.read())
 
 def get_db():
     db = sqlite3.connect("database.db")
