53b2f3a4a55b9c0e704a9cb2d1363d91a772ba18
Y00N-Park <Y00N-Park@users.noreply.github.com>
2022-05-04 21:08:59 -0700

    add form commit

--- adddelete.py
+++ adddelete.py
@@ -6,10 +6,10 @@ from wtforms.validators import DataRequired
 
 adddelete = Blueprint('adddelete', __name__)
 
-
 # create a Form Class
 class EventForm(FlaskForm):
     title = StringField("What is your event?", validators=[DataRequired()])
+    description = StringField("Please type your event details", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 #@adddelete.route('/')
@@ -20,14 +20,18 @@ class EventForm(FlaskForm):
 @adddelete.route('/add', methods=['GET', 'POST'])
 def add():
     title = None
+    description = None
     form = EventForm()
 
     if form.validate_on_submit():
         title = form.title.data
+        description = form.description.data
         form.title.data = ''
+        form.description.data = ''
 
     return render_template("add.html",
         title = title,
+        description = description,
         form = form)
 
 
