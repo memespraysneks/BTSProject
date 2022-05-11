c39839fc9e32bb61194426d09fdebe2b2f8ee842
Y00N-Park <Y00N-Park@users.noreply.github.com>
2022-05-10 20:09:04 -0700

    wtform-login-frontend

--- login.py
+++ login.py
@@ -6,29 +6,50 @@ from flask import redirect
 from flask import session
 from flask import url_for
 from dbconnection import get_db
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import sqlite3
 import json
 
 loginpage = Blueprint('loginpage', __name__)
 
+class UserForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()], render_kw={"placeholder": "username"})
+    password = StringField("Password", validators=[DataRequired()], render_kw={"placeholder": "password"})
+    login = SubmitField("Login")
+
 @loginpage.route("/login", methods=('GET', 'POST'))
-def runTheData ():
+def login():
+    username = None
+    password = None
+    form = UserForm()
+
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        error = None
-        user = get_db().execute(
-            'SELECT * FROM USERS WHERE USERNAME = ?', (username,)
-        ).fetchone()
+        if form.validate_on_submit():
+            username = form.username.data
+            password = form.password.data
+            
+            form.username.data = ''
+            form.password.data = ''
+            
+            error = None
+            user = get_db().execute(
+                'SELECT * FROM USERS WHERE USERNAME = ?', (username,)
+            ).fetchone()
 
-        if user is None:
-            error = 'Incorrect username.'
-        elif not user['USERPASSWORD'] == password:
-            error = 'Incorrect password.'
 
-        if error is None:
-            session.clear()
-            session['user_id'] = user['USERID']
-            return redirect("/month")
+            if user is None:
+                error = 'Incorrect username.'
+            elif not user['USERPASSWORD'] == password:
+                error = 'Incorrect password.'
 
-    return render_template('login.html')
+            if error is None:
+                session.clear()
+                session['user_id'] = user['USERID']
+                return redirect("/month")
+
+    return render_template('login.html',
+        username = username,
+        password = password,
+        form = form)
